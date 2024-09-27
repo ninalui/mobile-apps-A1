6df341d7c77cb693dfb847c6835b8f4a5298235c
nina <lui.n@northeastern.edu>
2024-09-27 06:57:02 -0700

    stylized Game.js to be card, moved restart button to be outside of card

--- screens/game/Game.js
+++ screens/game/Game.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 import GamePrompts from "../../components/GamePrompts";
 
-export default function Game( {guessMultiple} ) {
+export default function Game({ guessMultiple }) {
   const [isGameStarted, setIsGameStarted] = useState(false);
 
   function startGame() {
@@ -10,16 +10,37 @@ export default function Game( {guessMultiple} ) {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
+
       {/* take user back to start screen and reset all information */}
       <Button title="Restart" />
 
-      {/* guessing anumber that is a multiple of the last digit of user phone number */}
-      <Text>Guess a number between 1 & 100 that is multiply of {guessMultiple}</Text>
+      <View style={styles.card}>
 
-      {/* if game is started, show prompts, otherwise show start button */}
-      { isGameStarted ? <GamePrompts /> : <Button title="Start" onPress={startGame} /> }
+        {/* guessing anumber that is a multiple of the last digit of user phone number */}
+        <Text>Guess a number between 1 & 100 that is multiply of {guessMultiple}</Text>
 
+        {/* if game is started, show prompts, otherwise show start button */}
+        {isGameStarted ? <GamePrompts /> : <Button title="Start" onPress={startGame} />}
+      
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    padding: 10,
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'lightgrey',
+    backgroundColor: 'lightgrey',
+  }
+});
+
