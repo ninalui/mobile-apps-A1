f82a8d5d44f879c24cfaaf6d036424c2d1c2a811
nina <lui.n@northeastern.edu>
2024-09-27 20:33:17 -0700

    added generating random number when game starts

--- screens/game/Game.js
+++ screens/game/Game.js
@@ -7,6 +7,16 @@ export default function Game({ guessMultiple }) {
 
   function startGame() {
     setIsGameStarted(true);
+    getRandomNumber();
+  };
+
+  // set a random number to guess that is a multiple of the last digit of user phone number
+  const [number, setNumber] = useState(getRandomNumber());
+
+  function getRandomNumber() {
+    const maxMultiple = Math.floor(100 / guessMultiple);
+    const mutiplier = Math.floor(Math.random() * maxMultiple) + 1;
+    return mutiplier * guessMultiple;
   };
 
   return (
@@ -18,7 +28,7 @@ export default function Game({ guessMultiple }) {
       <View style={styles.card}>
 
         {/* guessing anumber that is a multiple of the last digit of user phone number */}
-        <Text>Guess a number between 1 & 100 that is multiply of {guessMultiple}</Text>
+        <Text>Guess a number between 1 & 100 that is multiple of {guessMultiple}</Text>
 
         {/* if game is started, show prompts, otherwise show start button */}
         {isGameStarted ? <GamePrompts /> : <Button title="Start" onPress={startGame} />}
